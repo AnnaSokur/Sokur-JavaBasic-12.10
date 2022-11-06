5b4d250c1e3ca2b40e2475041f5f0f47d45d174d
Anna Sokur <115190892+AnnaSokur@users.noreply.github.com>
2022-11-06 20:16:22 +0100

    HomeTask10

---
+++ Lottery.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lottery {
+    public static void main(String[] args) {
+        int first[] = new int[7];
+        for (int i = 0; i < first.length; i++) {
+            first[i] = (int) (Math.random() * 10);
+        }
+        Arrays.sort(first);
+
+
+        System.out.println("Введите 7 цифр от 0 до 9: ");
+        Scanner in = new Scanner(System.in);
+        int second[] = new int[7];
+        for (int i = 0; i < second.length; i++) {
+            second[i] = in.nextInt();
+
+        }
+        Arrays.sort(second);
+        System.out.print(Arrays.toString(first));
+        System.out.println("");
+        System.out.print(Arrays.toString(second));
+        System.out.println();
+
+
+        int count = 0;
+        for (int i = 0; i < second.length; i++) {
+            if (first[i] == second[i]) count++;
+        }
+        System.out.println("Количество совпадений: " + count);
+    }
+}
